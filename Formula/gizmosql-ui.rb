9b888520ee412940d9d7937bbe3ad79be61b2ab3
Philip Moore <philip@gizmodata.com>
2026-07-29 11:40:39 -0400

    Revert "Update gizmosql-ui to 2.6.0"
    This reverts commit c9f001f2382899ef1afec7db572339c5417721bc.

--- Formula/gizmosql-ui.rb
+++ Formula/gizmosql-ui.rb
@@ -1,13 +1,13 @@
 class GizmosqlUi < Formula
   desc "Web-based SQL interface for GizmoSQL servers"
   homepage "https://github.com/gizmodata/gizmosql-ui"
-  version "2.6.0"
+  version "2.5.8"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/gizmodata/gizmosql-ui/releases/download/v#{version}/gizmosql-ui-macos-arm64"
-      sha256 "e4dda51c82f91ad0191aff5b65f2f8955cc48151831947bbfbacbb4691023033"
+      sha256 "613f14218e752e089a22d6abf105681cfb1384efbedae464a92cab89f3d1aee7"
 
       def install
         bin.install "gizmosql-ui-macos-arm64" => "gizmosql-ui"
@@ -18,14 +18,14 @@ class GizmosqlUi < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/gizmodata/gizmosql-ui/releases/download/v#{version}/gizmosql-ui-linux-arm64"
-      sha256 "c80b97c56711a5a9fa6c2c63dcc8601678f728859b4defec7c045c7fcf3ca8f1"
+      sha256 "ba7cf05de6a405dfb38cad37fa6199c1b1c119fb55a638d9774d001e7d7a006b"
 
       def install
         bin.install "gizmosql-ui-linux-arm64" => "gizmosql-ui"
       end
     elsif Hardware::CPU.intel?
       url "https://github.com/gizmodata/gizmosql-ui/releases/download/v#{version}/gizmosql-ui-linux-x64"
-      sha256 "8cb1687d3893e0a7031fb99534bcccf9cebdb54be7ba7234ff33c732dc237a29"
+      sha256 "ccf28850260c191759bebce547b7c8c2b0230473f79a5d4404b09b1eec19d831"
 
       def install
         bin.install "gizmosql-ui-linux-x64" => "gizmosql-ui"
